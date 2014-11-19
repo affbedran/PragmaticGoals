cfd8c2dd464727a1738bd6d79f9f97765fd6dcbb
Felipe Pontes <fpontes100@gmail.com>
2014-11-19 16:51:58 -0200

    Minor improvements

--- src/test/java/workflow/WorkflowGeneration.java
+++ src/test/java/workflow/WorkflowGeneration.java
@@ -16,7 +16,7 @@ public class WorkflowGeneration {
 		int nodes = 1, averageEdgesPerNode = 1;
 		Workflow wf = null;
 
-		for (nodes = 1; nodes < 100; nodes++) {
+		for (nodes = 1; nodes <= 100; nodes++) {
 
 			wf = wfFactory.createRandomWorkflow(nodes, averageEdgesPerNode);
 			assertTrue(wf != null);
@@ -32,7 +32,7 @@ public class WorkflowGeneration {
 		int nodes = 1, averageEdgesPerNode = 3;
 		Workflow wf = null;
 
-		for (nodes = 1; nodes < 100; nodes++) {
+		for (nodes = 1; nodes <= 100; nodes++) {
 
 			wf = wfFactory.createRandomWorkflow(nodes, averageEdgesPerNode);
 			assertTrue(wf != null);
@@ -61,7 +61,8 @@ public class WorkflowGeneration {
 			if (found == 0)
 				unconnectedNodes++;
 		}
-		assertEquals(1, unconnectedNodes);
+		assertEquals(1, unconnectedNodes); // Only the root node may not have
+											// any incoming edges
 	}
 
 }
